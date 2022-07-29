0d60cda524922ad9b81d5f4df9a2d69479082fdf
Tyler Nijmeh <tylernij@gmail.com>
2022-07-29 14:27:37 -0700

    Default to light mode
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/themes.dart
+++ lib/themes.dart
@@ -5,9 +5,6 @@ const darkGray = Color.fromARGB(255, 33, 33, 33);
 
 /// The main [ThemeData] to use for the app.
 final appTheme = ThemeData(
-  brightness: Brightness.dark,
   primarySwatch: Colors.grey,
   primaryColor: darkGray,
-  backgroundColor: darkGray,
-  scaffoldBackgroundColor: darkGray,
 );
